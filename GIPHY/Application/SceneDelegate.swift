745ce4c970d3e9183981afc7af1733aa351ec5c1
sanghunlee <simoniful0@gmail.com>
2022-08-18 12:30:02 +0900

    Add: 기본 세팅 (2)
    Code based UI 구성을 위한 준비 - 스토리보드 요소 제거
    Pod을 통한 서드파티 모듈 구성

---
+++ GIPHY/Application/SceneDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  SceneDelegate.swift
+//  GIPHY
+//
+//  Created by Sang hun Lee on 2022/08/18.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,29 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.0'
+
+target 'GIPHY' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GIPHY
+  pod 'SnapKit'
+  pod 'Alamofire'
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'RxBlocking'
+  pod 'RxAlamofire'
+  pod 'Toast-Swift'
+  target 'GIPHYTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'Stubber'
+    pod 'Nimble'
+    pod 'RxBlocking'
+    pod 'RxTest'
+  end
+
+  target 'GIPHYUITests' do
+    # Pods for testing
+  end
+
+end
